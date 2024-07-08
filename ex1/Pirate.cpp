a7fd1e174aa3c81c120f419d117c354cb0baf8fb
boazmr <158840378+boazmr@users.noreply.github.com>
2024-07-08 17:00:15 +0000

    update Pirate.cpp

--- ex1/Pirate.cpp
+++ ex1/Pirate.cpp
@@ -1,71 +1,23 @@
 #include "Pirate.h"
 #include "Ship.h"
 
-// Class representing a Pirate
 class Pirate {
 public:
-    /**
-     * Constructor to initialize a Pirate object.
-     * @param pirateId - The unique ID of the pirate.
-     * @param treasure - The amount of treasure the pirate possesses.
-     * Note: The shipId is not used in the Pirate class.
-     *       The caller to the constructor will update the Pirate->ship field.
-     */
     Pirate(int pirateId, int treasure) 
         : id(pirateId), treasure(treasure), ship(nullptr), prev_pirate(nullptr), next_pirate(nullptr) {}
-
-    /**
-     * Sets the amount of treasure the pirate possesses.
-     * @param treasure - The new amount of treasure.
-     */
-    void setTreasure(int treasure);
-
-    /**
-     * Gets the amount of treasure the pirate possesses.
-     * @return The amount of treasure.
-     */
-    int getTreasure() const;
-
-    /**
-     * Sets the ship the pirate is associated with.
-     * @param ship - Pointer to the AVLNode containing the ship.
-     */
-    void setShip(AVLNode<Ship>* ship);
-
-    /**
-     * Gets the ship the pirate is associated with.
-     * @return Pointer to the AVLNode containing the ship.
-     */
-    AVLNode<Ship>* getShip() const;
-
-    /**
-     * Sets the next pirate in the linked list.
-     * @param next_pirate - Pointer to the next pirate.
-     */
-    void setNext(Pirate* next_pirate);
-
-    /**
-     * Gets the next pirate in the linked list.
-     * @return Pointer to the next pirate.
-     */
-    Pirate* getNext();
-
-    /**
-     * Sets the previous pirate in the linked list.
-     * @param prev_pirate - Pointer to the previous pirate.
-     */
-    void setPrev(Pirate* prev_pirate);
-
-    /**
-     * Gets the previous pirate in the linked list.
-     * @return Pointer to the previous pirate.
-     */
-    Pirate* getPrev();
+    void setTreasure(int treasure) { this->treasure = treasure;}
+    int getTreasure() const { return treasure;}
+    void setShip(AVLNode<Ship>* ship) {this->ship = ship;}
+    AVLNode<Ship>* getShip() const {return ship;}
+    void setNext(Pirate* next_pirate) {this->next_pirate = next_pirate;}
+    Pirate* getNext() const {return next_pirate;}
+    void setPrev(Pirate* prev_pirate) {this->prev_pirate = prev_pirate;}
+    Pirate* getPrev() const { return prev_pirate;}
 
 private:
-    int id;                    ///< The unique ID of the pirate.
-    int treasure;              ///< The amount of treasure the pirate possesses.
-    AVLNode<Ship>* ship;       ///< Pointer to the AVLNode containing the ship the pirate is associated with.
-    Pirate* next_pirate;       ///< Pointer to the next pirate in the linked list.
-    Pirate* prev_pirate;       ///< Pointer to the previous pirate in the linked list.
+    int id;
+    int treasure;
+    AVLNode<Ship>* ship;
+    Pirate* next_pirate;
+    Pirate* prev_pirate;
 };
