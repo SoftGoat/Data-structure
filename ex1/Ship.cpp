b5ec5a560954da12200acbf47c82f9425d7f4b50
SoftGoat <118727861+SoftGoat@users.noreply.github.com>
2024-07-08 17:46:26 +0000

    Developing ship

--- ex1/Ship.cpp
+++ ex1/Ship.cpp
@@ -1,6 +1,7 @@
 #include "../AVLTree_boaz.h"
 #include <iostream>
 #include "Pirate.h"
+#include "Ship.h"
 #include "wet1util.h"
 class Pirate; // Forward declaration of the Pirate class
 
@@ -9,28 +10,90 @@ class Ship{
 private:
     int m_shipId;
     int m_cannons;
-    int m_treasure;
-    AVLTree<Pirate> m_pirates;
+    int m_treasureBonus;
+    int crewSize;
+    AVLTree<Pirate*> m_pirates;
     Pirate* m_richestPirate;
+    Pirate* m_pirateWithMostTimeServed;
+    Pirate* m_pirateWithLeastTimeServed;
+    AVLTree<Pirate*>* m_piratesOrderdById;
+    AVLTree<Pirate*>* m_piratesOrderdByTreasure;
+
 
 public:
-    StatusType add_ship(int shipId, int cannons){
-
+    Ship(int shipId, int cannons,AVLTree<Pirate*>* piratesOrderdById, AVLTree<Pirate*>* piratesOrderdByTreasure){
+        m_shipId = shipId;
+        m_cannons = cannons;
+        m_treasureBonus = 0;
+        crewSize = 0;
+        m_richestPirate = nullptr;
+        m_pirateWithMostTimeServed = nullptr;
+        m_pirateWithLeastTimeServed = nullptr;
+        m_piratesOrderdById = piratesOrderdById;
+        m_piratesOrderdByTreasure = piratesOrderdByTreasure;
     }
-    StatusType remove_ship(int shipId){
-
+    ~Ship(){
+    
     }
-    StatusType add_pirate(int pirateId , int shipId, int treasure){
-
+    StatusType add_pirate(Pirate* pirate){
+        m_pirates.insert(pirate);
+        if(crewSize == 0 || pirate->getTreasure() > m_richestPirate->getTreasure()){
+            m_richestPirate = pirate;
+        }
+        if(crewSize == 0){
+            m_pirateWithMostTimeServed = pirate;
+            m_pirateWithLeastTimeServed = pirate;
+        }
+        else{
+            m_pirateWithLeastTimeServed->setNext(pirate);
+            m_pirateWithLeastTimeServed = pirate;
+        }
+        crewSize++;
+        return StatusType::SUCCESS;
     }
-    StatusType remove_pirate(int pirateId){
+    StatusType remove_pirate(Pirate* pirate){
+        if(crewSize == 0){
+            return StatusType::FAILURE;
+        }
+        if(pirate == m_pirateWithMostTimeServed){
+            m_pirateWithMostTimeServed = pirate->getNext();
+        }
+        else{
+            pirate->getPrev()->setNext(pirate->getNext());        
+        }
+        m_pirates.remove(pirate);
+        if(m_richestPirate == pirate){
+            m_richestPirate = nullptr;
+            if(!m_pirates.isEmpty()){
+                m_richestPirate = m_pirates.findMaxNode()->getData();
+            }
+        }
+
+        crewSize--;
+        return StatusType::SUCCESS;
 
     }
     StatusType treason(int sourceShipId, int destShipId){
 
     }
-    StatusType update_pirate_treasure(int pirateId , int change){
+    StatusType update_pirate_treasure(Pirate* pirate , int change){
+        
+    }
 
+    int getShipId(){
+        return m_shipId;
+    }
+    int getCannons(){
+        return m_cannons;
+    }
+    int treasureBonus(){
+        return m_treasureBonus;
+    }
+    Pirate* getRichestPirate(){
+        return m_richestPirate;
+    }
+    AVLTree<Pirate*> getPirates(){
+        return m_pirates;
     }
 
 
